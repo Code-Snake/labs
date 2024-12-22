e98efdd47e1c5b7c114e8c80e334db41d0fdd807
Anastasia Komarova <120313921+Code-Snake@users.noreply.github.com>
2024-12-23 01:37:58 +0700

    point

---
+++ Numerical Methods/Lab_8/Point.h
@@ -0,0 +1,14 @@
+#pragma once
+
+	//точка
+class Point{
+	private:
+		double X, Y, Z;
+	public:
+		Point(double x, double y, double z);
+		double x()const;
+		double y()const;
+		double z()const;
+};
+
+
